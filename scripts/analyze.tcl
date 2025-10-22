6a9cab54d8dd23b6f061c048ce5c47815447d850
Jeffryin <74562726+Jeffryin@users.noreply.github.com>
2025-10-21 17:10:15 -0700

    Enhance analyze.tcl with user input deciding whether to elaborate or read
    Added user prompts for elaboration or reading of RTL files.

--- scripts/analyze.tcl
+++ scripts/analyze.tcl
@@ -4,6 +4,17 @@ puts -nonewline "Enter RTL filename(s): "
 flush stdout
 gets stdin RTL_FILE
 
-analyze -library WORK -format Verilog $RTL_FILE
+analyze -library WORK -format Verilog $RTL_FILE  
 
-read_verilog -rtl $RTL_FILE
+puts -nonewline "Elaborate or read RTL file(s)?"
+flush stdout
+gets stdin RESPONSE
+
+if {$RESPONSE eq "elaborate"}{
+  puts -nonewline "Enter top module you'd like to sythesize: "
+  flush stdout
+  gets stdin TOP
+  elaborate -architecture Verilog -library WORK $TOP
+}elseif {$RESPONSE eq "read"}{  
+  read_verilog -rtl $RTL_FILE
+}
