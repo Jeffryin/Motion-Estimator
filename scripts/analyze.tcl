61c623f45fb440e3ff6ab3c90fa15046773aec13
Jeffryin <74562726+Jeffryin@users.noreply.github.com>
2025-10-22 16:15:44 -0700

    Fix syntax for conditional checks in analyze.tcl

--- scripts/analyze.tcl
+++ scripts/analyze.tcl
@@ -10,11 +10,11 @@ puts -nonewline "Elaborate or read RTL file(s)?"
 flush stdout
 gets stdin RESPONSE
 
-if {$RESPONSE eq "elaborate"}{
+if {$RESPONSE eq "elaborate"} {
   puts -nonewline "Enter top module you'd like to sythesize: "
   flush stdout
   gets stdin TOP
   elaborate -architecture Verilog -library WORK $TOP
-}elseif {$RESPONSE eq "read"}{  
+} elseif {$RESPONSE eq "read"} {  
   read_verilog -rtl $RTL_FILE
 }
