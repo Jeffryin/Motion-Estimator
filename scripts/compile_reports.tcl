a8fc7a5842b83e0eed2832872f478c33c84f5b5d
Jeffryin <74562726+Jeffryin@users.noreply.github.com>
2025-10-24 17:51:52 -0700

    Update report file names to include Output_Name
    concatenate the Output Name so every report file is not being overwritten for each run

--- scripts/compile_reports.tcl
+++ scripts/compile_reports.tcl
@@ -40,12 +40,12 @@ write_sdc ./outputs/$Output_Name.sdc
 write –f ddc –hierarchy –output ./outputs/$Output_Name.ddc
 write -hierarchy -format verilog -output ./outputs/$Output_Name.v
 
-report_area > reports/synth_area.rpt
-report_design > reports/synth_design.rpt
-report_cell > reports/synth_cells.rpt
-report_qor > reports/synth_qor.rpt
-report_power > reports/synth_power.rpt
-report_timing > reports/setup.rpt
-report_timing -delay min > reports/hold.rpt
+report_area > reports/${Output_Name}_synth_area.rpt
+report_design > reports/${Output_Name}_synth_design.rpt
+report_cell > reports/${Output_Name}_synth_cells.rpt
+report_qor > reports/${Output_Name}_synth_qor.rpt
+report_power > reports/${Output_Name}_synth_power.rpt
+report_timing > reports/${Output_Name}_setup.rpt
+report_timing -delay min > reports/${Output_Name}_hold.rpt
 
 //should be able to start gui afterwards. with the command start_gui
