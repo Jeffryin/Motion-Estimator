c745603b426bac3b1371884e2b1c4b4b1de8bdf3
Jeffryin <74562726+Jeffryin@users.noreply.github.com>
2025-11-06 11:33:31 -0800

    Refactor PTsetup.tcl for better user interaction
    Updated PTsetup.tcl to improve user prompts and error handling.

--- scripts/PTsetup.tcl
+++ scripts/PTsetup.tcl
@@ -1,4 +1,5 @@
-# use source /home/ID#/PTsetup.tcl
+# use source /home/ID#/PTsetup.tcl 
+
 source /packages/synopsys/setup/asic_flow_setup_14/lib_gen/pt_lib.tcl
 
 puts -nonewline "Enter filepath where .v (Netlist) is located: "
@@ -11,19 +12,26 @@ gets stdin NET
 
 read_verilog "$FP$NET"
 
+puts -nonewline "Enter name of top module: "
+flush stdout
+gets stdin TM 
+
+current_design $TM
+
 link_design 
 
-current_design
-
-puts -nonewline "STA? y/n: "
+puts -nonewline "Enter name of SDC file with .sdc: "
 flush stdout 
-gets stdin STA
+set SDCF [string trim [gets stdin]]
 
-if {STA eq yes}{
-	set_case_analysis 1 -in scanEn
-	}elseif{
-	set_case_analysis 0 -in scanEn	
+set sdc_path [file join $FP $SDCF]
+
+if {![file exists $sdc_path]} {
+	puts "ERROR: File not found: $FP"
+	exit 1
 }
+
+read_sdc $sdc_path
 
 puts -nonewline "Enter name for reports: (PT_setup...rpt)"
 flush stdout 
