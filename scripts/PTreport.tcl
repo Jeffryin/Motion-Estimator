64b363c7b5c4117683a614248054dc5427292cd5
Jeffryin <74562726+Jeffryin@users.noreply.github.com>
2025-11-19 13:30:38 -0800

    Add user input for report file prefix

--- scripts/PTreport.tcl
+++ scripts/PTreport.tcl
@@ -1,7 +1,11 @@
-report_timing -from [all_inputs] -to [all_registers -data_pins] > reports/in2reg_setup.rpt
+puts -nonewline "Enter prefix for the file: "
+flush stdout 
+gets stdin PRE
 
-report_timing -from [all_registers -clock_pins] -to [all_registers -data_pins] > reports/reg2reg_setup.rpt
+report_timing -from [all_inputs] -to [all_registers -data_pins] > reports/${PRE}_in2reg_setup.rpt
 
-report_timing -from [all_registers -clock_pins] -to [all_outputs] > reports/reg2out_setup.rpt
+report_timing -from [all_registers -clock_pins] -to [all_registers -data_pins] > reports/${PRE}reg2reg_setup.rpt
 
-report_timing > reports/setup.rpt
+report_timing -from [all_registers -clock_pins] -to [all_outputs] > reports/${PRE}reg2out_setup.rpt
+
+report_timing > reports/${PRE}setup.rpt
