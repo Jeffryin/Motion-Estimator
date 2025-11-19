d2023fc8591cbd5c0a46c74dbb14beb49dc16b05
Jeffryin <74562726+Jeffryin@users.noreply.github.com>
2025-11-19 15:38:50 -0800

    Improve prompts for user input in structure.tcl

--- scripts/structure.tcl
+++ scripts/structure.tcl
@@ -1,19 +1,19 @@
 #set_structure <true | false> -design <list of designs> -boolean <low | medium | high> -timing <true | false> 
 
-puts -nonewline "Would you like to structure? [true false]"
+puts -nonewline "Would you like to structure? true false: "
 flush stdout 
 gets stdin STRUCT
 
 if {$STRUCT eq "true"} { 
-	puts -nonewline "List all design(s): "
+	puts -nonewline "List collection of designs (run some commands to filter, ex: get_designs -hier -filter...: "
 	flush stdout 
 	gets stdin DESIGN
 
-	puts -nonewline "Boolean level [low medium high]"
+	puts -nonewline "Boolean level true false: "
 	flush stdout
 	gets stdin BOOL
 
-	puts -nonewline "Set timing [true false]"
+	puts -nonewline "Set timing true false: "
 	flush stdout
 	gets stdin TIME
 
