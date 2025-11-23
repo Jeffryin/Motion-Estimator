41933b4d905ef87085d1dde625291989264aef9f
Jeffryin <74562726+Jeffryin@users.noreply.github.com>
2025-11-23 00:23:46 -0800

    Parameterize file names in PTpost.tcl script

--- scripts/PTpost.tcl
+++ scripts/PTpost.tcl
@@ -1,22 +1,26 @@
 source /packages/synopsys/setup/asic_flow_setup_14/lib_gen/pt_lib.tcl
 
-read_verilog /home/ID-number/asic_flow_setup_14/pnr/outputs/ME.v
+puts -nonewline "Enter name of the file: "
+flush stdout 
+gets stdin file
+read_verilog /home/920528346/asic_flow_setup_14/pnr/outputs/${file}.v
 
 link_design
 
 current_design
 
-read_parasitics -format SPEF /home/ID-number/asic_flow_setup_14/pnr/outputs/ME.spef.tlup_max_125.spef
+read_parasitics -format SPEF /home/920528346/asic_flow_setup_14/pnr/outputs/${file}.spef.tlup_max_125.spef
 
-read_sdc /home/ID-number/asic_flow_setup_14/pnr/outputs/ME.sdc
+read_sdc /home/920528346/asic_flow_setup_14/pnr/outputs/${file}.sdc
 
-report_timing > reports/MEsetup.rpt
-report_timing -delay_type min > reports/MEhold.rpt
+report_timing > reports/${file}setup.rpt
+report_timing -delay_type min > reports/${file}hold.rpt
 
-read_parasitics -format SPEF /home/ID-number/asic_flow_setup_14/pnr/outputs/ME.spef.tlup.min_125.spef
-read_sdc /home/ID-number/asic_flow_setup_14/pnr/outputs/ME.sdc
-report_timing -delay_type min > reports/MEhold.rpt
+read_parasitics -format SPEF /home/920528346/asic_flow_setup_14/pnr/outputs/${file}.spef.tlup.min_125.spef
+read_sdc /home/920528346/asic_flow_setup_14/pnr/outputs/${file}.sdc
 
-report_timing -from [all_registers -clock_pins] -to [all_registers -data_pins] > reports/MEreg2reg_setup.rpt
+report_timing -delay_type min > reports/${file}hold.rpt
 
-report_timing -from [all_registers -clock_pins] -to [all_registers -data_pins] -delay_type min > reports/MEreg2reg_hold.rpt
+report_timing -from [all_registers -clock_pins] -to [all_registers -data_pins] > reports/${file}reg2reg_setup.rpt
+
+report_timing -from [all_registers -clock_pins] -to [all_registers -data_pins] -delay_type min > reports/${file}reg2reg_hold.rpt
